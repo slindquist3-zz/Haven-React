64247f6a3740093f07627b85d5eb710ae5115dce
slindquist3 <slindquist3@gmail.com>
2017-02-27 15:14:59 -0500

    h5

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,9 @@ export default class About extends Component {
           <h3 className="center"></h3>
           <h3 className="center">Haven offers a quiet space.</h3>
           <h4 className="center"></h4>
-          <h4 className ="center"> Built with a minimalist approach to social media,{"\n"}
-            Haven is a platform to connect one-on-one with the people who matter most to you.{"\n"}
-            Create and share content free from ads, algorithms, and trends.</h4>
+          <h5 className ="center"> Built with a minimalist approach to social media,
+            Haven is a platform to connect one-on-one with the people who matter most to you.
+            Create and share content free from ads, algorithms, and trends.</h5>
         </Col>
       </div>
 
