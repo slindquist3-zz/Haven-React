87a615adc77ad5c2d425a5947d04330cc3254018
slindquist3 <slindquist3@gmail.com>
2017-02-27 10:27:35 -0500

    formatting columns

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Row, Col} from 'react-bootstrap'
 
 export default class About extends Component {
 
@@ -7,8 +8,11 @@ export default class About extends Component {
 
       <div>
         <p className="logoWelcome">Haven</p>
-        <h3 className="center">Haven offers a quieter space for people to connect.
-          Create a board and post content without being interrupted by ads and other internet noise.</h3>
+        <Col className="col-md-8 col-md-offset-2"
+          <h3 className="center">Haven offers a quieter space for people to connect.
+            Create a board and post content without being interrupted by ads and other internet noise.
+          </h3>
+        </Col>
       </div>
 
     )
