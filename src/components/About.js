5cc26926f1d9acbbdd241d99d7b49c11c7e244ab
slindquist3 <slindquist3@gmail.com>
2017-02-27 10:34:35 -0500

    editing copy

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,9 @@ export default class About extends Component {
 
       <div>
         <p className="logoWelcome">Haven</p>
-        <Col className="col-md-8 col-md-offset-2">
-          <h3 className="center">Haven offers a quieter space for people to connect.
-            Create a board and post content without being interrupted by ads and other internet noise.
-          </h3>
+        <Col className="col-md-8 col-md-offset-4">
+          <h3 className="center">Haven offers a quieter space for people to connect.</h3>
+          <h3 className ="center">Create a board and post content without being interrupted by ads and other internet noise.</h3>
         </Col>
       </div>
 
