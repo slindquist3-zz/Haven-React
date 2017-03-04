1887ff72bc8bbc968f9bb275f17f6d1c3d868984
slindquist3 <slindquist3@gmail.com>
2017-03-03 21:55:45 -0500

    Reverted just in case

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -35,7 +35,7 @@ class Welcome extends Component {
         <div>
 
         <Row>
-          <p className="logo text-center fade">test</p>
+          <p className="logo text-center fade">HAVEN</p>
         </Row>
         <Row>
           <Col className="col-md-8 col-md-offset-2">
